3c11144f5e1eddf0127f72c18b1f520d635bbbf4
acostacion <carmengb27@ucm.es>
2024-10-08 16:51:24 +0200

    hola

--- PVLI/Entrega DOM y Canvas/Ejercicio 2/main.js
+++ PVLI/Entrega DOM y Canvas/Ejercicio 2/main.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     var button = document.getElementById('summon');
     button.addEventListener('click', function () {
-        document.getElementById('ankisses').innerHTML = 
+        document.getElementById('ankisses').innerHTML += 
         " <img src = 'https://pbs.twimg.com/media/FS2FTv7WUAAPta0.jpg' alt = 'frosmor'>" ;
     });
 };
